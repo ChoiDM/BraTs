fbc37557fa88bf120126d1e871af00a82dbf7a24
dongmin-choi <dongmin.choi@navercorp.com>
2020-07-21 18:46:57 +0900

    rm data

--- train.py
+++ train.py
@@ -24,34 +24,33 @@ torch.cuda.manual_seed(0)
 np.random.seed(0)
 random.seed(0)
 
-# Option
-opt = parse_option(print_option=True)
-
-# Data Loader
-dataset_trn, dataset_val = get_dataloader(opt)
-
-# Network
-net = create_model(opt)
-
-# Loss Function
-criterion = get_loss_function(opt)
-
-# Optimizer
-optimizer = get_optimizer(net, criterion, opt)
-scheduler = CosineAnnealingLR(optimizer, eta_min=opt.lr*opt.eta_min_ratio, T_max=(opt.max_epoch - opt.lr_warmup_epoch))
-
-# Initial Best Score
-best_dice, best_epoch = [0, 0]
-
-    
 #NOTE: main loop for training
 if __name__ == "__main__":
+    # Option
+    opt = parse_option(print_option=True)
+
+    # Data Loader
+    dataset_trn, dataset_val = get_dataloader(opt)
+
+    # Network
+    net = create_model(opt)
+
+    # Loss Function
+    criterion = get_loss_function(opt)
+
+    # Optimizer
+    optimizer = get_optimizer(net, criterion, opt)
+    scheduler = CosineAnnealingLR(optimizer, eta_min=opt.lr*opt.eta_min_ratio, T_max=(opt.max_epoch - opt.lr_warmup_epoch))
+
+    # Initial Best Score
+    best_dice, best_epoch = [0, 0]
+
     for epoch in range(opt.start_epoch, opt.max_epoch):
         # Train
         train(net, dataset_trn, optimizer, criterion, epoch, opt)
 
         # Evaluate
-        best_dice, best_epoch = evaluate(dataset_val, net, criterion, optimizer, epoch, opt, best_dice, best_epoch)
+        best_dice, best_epoch = validate(dataset_val, net, criterion, optimizer, epoch, opt, best_dice, best_epoch)
 
         lr_update(epoch, opt, optimizer, scheduler)
 
